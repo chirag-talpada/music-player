b44661be604cef0605249fcf5dd999b9ed5501bd
chirag-talpada <chiragvaghela492@gmail.com>
2022-08-05 17:12:55 +0530

    music player

---
+++ music-list.js
@@ -0,0 +1,27 @@
+let allMusic = [
+    {
+      name: "Alone",
+      artist: "Alan Walker",
+      img: "music-1",
+      src: "music-1"
+    },
+    {
+      name: "Let Me Love You",
+      artist: "justin bieber",
+      img: "music-2",
+      src: "music-2"
+    },
+    {
+      name: "Anne-Marie - FRIENDS",
+      artist: "Marshmello",
+      img: "music-3",
+      src: "music-3"
+    },
+    {
+      name: "Despacito",
+      artist: "Luis Fonsi",
+      img: "music-4",
+      src: "music-4"
+    },
+ 
+  ];
